ba0a8c3f134cc880649c6d90de5a54b9403432ba
Eduardo Gabriel Kenzo Tanaka <eduduardogabriel888@gmail.com>
2024-04-29 19:07:40 -0300

    playlist embed

--- index.js
+++ index.js
@@ -108,4 +108,81 @@ volumeSllider.addEventListener("input", function() {
             changeSoundVolume(sound.querySelector("audio").id, value);
         }   
     });
-})
+});
+
+const defaultPlaylist = document.querySelector(".playlist-container").textContent;
+let addPlaylistButton = document.getElementById("add-playlist-button");
+
+function removePlaylistForm(playlistSection) {
+    console.log(playlistSection)
+
+    document.getElementById("add-playlist-section").innerHTML = playlistSection;
+
+    // Adiciona novamente o evento de clique ao botão "Adicionar Playlist"
+    let addPlaylistButton = document.getElementById("add-playlist-button");
+    addPlaylistButton = document.getElementById("add-playlist-button");
+    addPlaylistButton.addEventListener("click", addPlaylist);
+    
+};
+
+function addPlaylist() {
+    const playlistSection = document.getElementById("add-playlist-section").cloneNode(true);
+    const playlistSectionCopy = playlistSection.innerHTML;
+
+    let form = 
+    ` 
+    <form class="form-playlist" action="POST">
+    <label for="playlist-input"></label>
+    <textarea
+        id="input-playlist"
+        class="input-playlist" 
+        name="playlist-input" 
+        rows="5" 
+        cols="20" 
+        required
+        placeholder="Cole o codigo de incorporação da playlist aqui."></textarea>
+    <div>
+        <button class="button button-pill" id="button-playlist-submit" type="button">Enviar</button>
+        <button class="button button-pill" id="button-playlist-cancel" type="button">Cancelar</button>
+    </div>
+    </form>
+    `
+
+    let addPlaylistSection = document.getElementById("add-playlist-section");
+    addPlaylistSection.innerHTML = form;
+
+    console.log(addPlaylistSection.innerHTML)
+
+    // cancelamento do formulario de playlist
+    const cancelPlaylistButton = document.getElementById("button-playlist-cancel");
+    cancelPlaylistButton.addEventListener("click", function() {
+        removePlaylistForm(playlistSectionCopy)
+    });
+    
+    // enviar playlist personalizada
+    const submitPlaylistButton = document.getElementById("button-playlist-submit");
+    submitPlaylistButton.addEventListener("click", function() {
+        let customPlaylist = document.getElementById("input-playlist").value;
+        
+        let container = document.querySelector(".playlist-container");
+        container.style.backgroundColor = "transparent";
+        container.innerHTML = customPlaylist;
+
+        let iframe = document.querySelector(".playlist-container iframe");
+        iframe.style.height = "152px";
+    
+        removePlaylistForm(playlistSectionCopy);
+    });
+}
+
+addPlaylistButton.addEventListener("click", addPlaylist); 
+
+/* pequeno
+<iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/2j7iCxxDBAxpuZVKoApepZ?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy">
+</iframe> 
+*/
+
+/* grande
+<iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/2j7iCxxDBAxpuZVKoApepZ?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy">
+</iframe>
+*/
